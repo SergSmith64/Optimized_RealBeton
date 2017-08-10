c7d63614957319a4715f12d3bd53890fbe2aeea8
SergSmith64 <cheapthailand@gmail.com>
2017-08-10 23:54:00 +0400

    003-optimized

---
+++ app/mail_p.php
@@ -0,0 +1,14 @@
+<?php
+
+// $recepient = "realbeton@yandex.ru, sergsmith2008@yandex.ru";
+$recepient = "realbeton@yandex.ru, sergsmith2008@yandex.ru";
+$sitename = "РеалБетон";
+
+$subject = "СРОЧНО!!! Заказ ПЕСКОБЕТОНА с сайта \"$sitename\"";
+
+$name = trim($_POST["name"]);
+$phone = trim($_POST["phone"]);
+
+$message = "Имя: $name \nТелефон: $phone";
+
+mail($recepient, $subject, $message, "From: $frm_name <$email>" . "\r\n" . "Reply-To: $email" . "\r\n" . "X-Mailer: PHP/" . phpversion() . "\r\n" . "Content-type: text/html; charset=\"utf-8\"");
